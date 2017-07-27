d0ad99594413c52605ca3278c9b4f3cb396cfd5f
ivykamanchan <ivy_kmchan@yahoo.com>
2017-07-27 10:10:26 -0700

    Initial commit.

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).foundation();
+
+/*
+//$(document).ready(function() {
+$(document).foundation({
+    equalizer : {
+      // Specify if Equalizer should make elements equal height once they become stacked.
+      equalize_on_stack: true
+    }
+  });
+//});*/
+
+/*$('.tabs').on('toggled', function (event, tab) {
+    $(document).foundation('equalizer', 'reflow');
+});*/
+
