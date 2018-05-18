bc9391934e27620811279e893bf234cd0978bc66
ivykamanchan <ivy_kmchan@yahoo.com>
2018-05-18 15:04:44 -0700

    add kamantravel

---
+++ kamantravel/js/app.js
@@ -0,0 +1,25 @@
+$(document).foundation();
+
+/*$(document).foundation({
+  tabs: {
+    callback : function (tabs) {
+      $(window).trigger('resize');
+    }
+  }
+});
+
+$(document).foundation({
+  form: {
+    callback : function (form) {
+      $(window).trigger('resize');
+    }
+  }
+});
+
+$(document).foundation({
+  tabs: {
+    callback : function (tabs) {
+      $(window).trigger('resize');
+    }
+  }
+});*/
